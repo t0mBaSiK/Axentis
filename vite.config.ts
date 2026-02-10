2b7843098fa3bea7ad65b08bb9940366909a2f09
t0mBaSiK <78489630+t0mBaSiK@users.noreply.github.com>
2026-02-09 21:04:04 -0500

    Update Vite base configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import path from "path"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/axentis/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: "/",
 })
+
 
 
