710538a6200564fc826ea7b4a6fc820793c6fc44
t0mBaSiK <78489630+t0mBaSiK@users.noreply.github.com>
2026-02-10 01:50:27 +0000

    Fix build and configure GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
 export default defineConfig({
   plugins: [react()],
   base: "/axentis/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
+
 
